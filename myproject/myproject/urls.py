ee1a5a1bf8696d661b1247e922b3dfee66ccaa11
benxin1999 <benxin1999@Outlook.com>
2022-10-15 13:25:08 +0800

    URL changed
    URL changed

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("account/",include('account.urls')),
+    path("web-keeping/account/",include('account.urls')),
 ]
